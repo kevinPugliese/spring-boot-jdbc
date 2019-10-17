347f837228eaa06bb0e7c8c95c7452a08b10c7c0
Kevin Pugliese <kevin.lima.contato@gmail.com>
2019-10-17 00:05:56 -0300

    Config files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into person
+values(10001, 'Kevin', 'kevin.pugliese@gmail.com');
+
+insert into person
+values(10002, 'Maria', 'maria.lima@gmail.com');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table person
+(
+    id integer not null,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    primary key(id)
+);
